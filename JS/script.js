f39a1f69af8c37c4d4a06f363a2d356a54248322
Jesse <jessemcar556@gmail.com>
2021-12-10 13:28:26 -0800

    Added event lisener that takes the form inputs and logs it to console

--- JS/script.js
+++ JS/script.js
@@ -10,3 +10,18 @@ function togglemode() {
 const toggle = document.getElementById('toggle')
 
 toggle.addEventListener("click", togglemode)
+
+
+var SaveButton = document.getElementById("frm-btn")
+SaveButton.addEventListener("click", (evt) => {
+    namecontact = document.getElementById('name')[0].value
+    email = document.getElementById('email')[0].value
+    message = document.getElementById('msg')[0].value
+
+    NewContact = {
+        name: namecontact,
+        email: email,
+        message: address, 
+    }
+    console.log(NewContact)
+})
