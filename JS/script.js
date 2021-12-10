c2a064f9ecc8710cb02bdde56c74d4f17e068248
Jesse <jessemcar556@gmail.com>
2021-12-10 13:01:05 -0800

    added event listener to script that uses the togglemode() function when clicked

--- JS/script.js
+++ JS/script.js
@@ -5,16 +5,8 @@ function togglemode() {
         document.body.classList.toggle("dark-theme"); 
 }
 
-function setTheme(themeName) {
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
-}
 
 
-function toggleTheme() {
-    if (localStorage.getItem('theme') === 'dark-theme') {
-        setTheme('dark-theme');
-    } else {
-        setTheme('light-theme');
-    }
-}
+const toggle = document.getElementById('toggle')
+
+toggle.addEventListener("click", togglemode)
